2c1747d2e97eecd0dc34e8e0fea45156c0c60cb3
hancoro <robin.hancock@gmail.com>
2019-03-18 21:29:57 +0000

    Working Selenium Python script
    include draft wrapper. More work needed

--- PythonFile_RPH19_Selenium_PIP.py
+++ PythonFile_RPH19_Selenium_PIP.py
@@ -1,7 +1,46 @@
 # Selenium PIP was installed
 # Location is LIB\site-packages\selenium
 
-import selenium
+from selenium import webdriver
+url = "http://www.avakin.com/"
+results_folder = "C:\\Users\\hancoro\\PycharmProjects\\StuckInBebProject\\SeleniumResults\\"
+chrome_driver_location = "C:\\RPH_Drivers\\chromedriver.exe"
+
+# This creates the webdriver
+driver = webdriver.Chrome(chrome_driver_location)
+
+# This navigates to the the url specified
+driver.get(url)
+driver.set_page_load_timeout(10)
+
+
+def selenium_wrapper(xpath, action, sendkeytext):
+    try:
+        if action == "click":
+            driver.find_element_by_xpath(xpath).click()
+        elif action == "type":
+            driver.find_element_by_xpath(xpath).send_keys(sendkeytext)
+        elif action == "clear":
+            driver.find_element_by_xpath(xpath).clear()
+        else:
+            print("Selected action: '" + action + " is not included in the selenium wrapper")
+    except ZeroDivisionError as err:
+        print(err)
+    return ""
+
+
+#
+selenium_wrapper("//button[@id='catapultCookie']", "click", "")
+driver.maximize_window()
+driver.get_screenshot_as_file(results_folder + "Avakin_Landing_Page.png")
+selenium_wrapper("//li[@id='menu-item-67']/a", "click", "")
+driver.get_screenshot_as_file(results_folder + "Avakin_WhatsNew_Page.png")
+driver.close()
+driver.quit()
+
+
+
+
 
 
 
